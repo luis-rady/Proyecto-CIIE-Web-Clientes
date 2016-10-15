9e2a4deb27398ff1fae43870557633cdd2090d2c
LuisitoRady <crdluis97@gmail.com>
2016-10-14 21:27:04 -0500

    Update Login.php

--- Login.php
+++ Login.php
@@ -6,21 +6,19 @@
 <body>
 <div class="header">
 <a href=""><img src="https://scontent-dft4-2.xx.fbcdn.net/v/t34.0-12/14657689_1284081328281370_1603812898_n.jpg?oh=bc3f2ad7f15f562d02b1672464498ec8&oe=58023C67" alt="Logo" /></a>
-	<div class="modalfade" id="loginmodal">
-    	  <div class="modal-dialog">
-				<div class="loginmodal-container">
-					<h1>Login to Your Account</h1><br>
-				  <form action="Home.php" method="post">
-					<input type="text" name="user" placeholder="Username">
-					<input type="password" name="pass" placeholder="Password">
-					<input type="submit" name="login" class="login loginmodal-submit" value="Login">
-				  </form>
+	<div class="loginmodal">
+		<div class="loginmodal-container">
+			<h1>Login to Your Account</h1><br>
+			<form action="Home.php" method="post">
+				<input type="text" name="user" placeholder="Username">
+				<input type="password" name="pass" placeholder="Password">
+				<input type="submit" name="login" class="login loginmodal-submit" value="Login">
+			</form>
 					
-				  <div class="login-help">
-					<a href="Register.php">Register</a> - <a href="ForgotPass.php">Forgot Password</a>
-				  </div>
-				</div>
+			<div class="login-help">
+				<a href="Register.php">Register</a> - <a href="ForgotPass.php">Forgot Password</a>
 			</div>
-		  </div>
+		</div>
+	</div>
 </body>
 </html>
