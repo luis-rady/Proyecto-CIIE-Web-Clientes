c29ca7c259d94eec4f335383a5561a3e80582b7f
LuisitoRady <crdluis97@gmail.com>
2016-10-28 19:33:20 -0500

    Update Calendar.php

--- Calendar.php
+++ Calendar.php
@@ -7,8 +7,11 @@
 		<link href="//arshaw.com/js/fullcalendar-1.5.3/fullcalendar/fullcalendar.css" rel="stylesheet" />
 		<link href="http://arshaw.com/js/fullcalendar-1.5.3/fullcalendar/fullcalendar.print.css" rel="stylesheet" />
     <link href="Calendar.css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 		<!-- SCRIPTS -->
-        <script src='http://fullcalendar.io/js/fullcalendar-2.2.3/fullcalendar.min.js'></script>
+    <script src='http://fullcalendar.io/js/fullcalendar-2.2.3/fullcalendar.min.js'></script>
     <script class="cssdesk" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
 		<script class="cssdesk" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js" type="text/javascript"></script>
 		<script class="cssdesk" src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.1.0/js/bootstrap.min.js" type="text/javascript"></script>
@@ -16,10 +19,14 @@
 	</head>
 
 	<body>
-		<div class="container">
-			<h1>Date</h1>
+		<div class="w3-display-container w3-red w3-padding-16">
+			<h1>Calendar</h1>
+      <a href="Home.php" class="w3-btn w3-hover-white w3-display-topleft w3-margin">Back to Home</a>
 
-			<div id='calendar'></div>
+			<div class="w3-container w3-padding-16 w3-white w3-border w3-margin-left w3-margin-right">
+        <div id='calendar'></div>
+
+      </div>
 		</div>
 
     <script>
